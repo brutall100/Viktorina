f2628ea38e7acca1d4eb946dad1052ba24354e77
brutall100 <brutall100@gmail.com>
2021-07-20 14:59:10 +0300

    Pakurta beganti eilute
    Pakurta beganti eilute bus dar tobulinama.Bus po mygtukais prisijungimas ir registracija

--- regilogi.js
+++ regilogi.js
@@ -1,11 +1,11 @@
 function login() { 
-    document.getElementById("xxxxx").style.left = "-200px"; 
-    // document.getElementById("register-btn").style.left = "400px";
+    document.getElementById("register").style.left = "20px"; 
+    document.getElementById("login").style.left = "400px";
   }
-  // function register() {
-  //   // document.getElementById("login-btn").style.left = "-40px";
-  //   document.getElementById("register-btn").style.left = "40px";
-  // }  
+  function register() {
+    document.getElementById("login").style.left = "-40px";
+    document.getElementById("register").style.left = "40px";
+  }  
 
 //   var f = document.getElementById("login-btn"); 
 //   var g = document.getElementById("register-btn"); 
