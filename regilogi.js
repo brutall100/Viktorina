e54b645822b11c4d07a890ca7eb345ea28ebbb70
brutall100 <brutall100@gmail.com>
2021-07-20 12:18:13 +0300

    Pataisyta reistracija
    Truputi aptavarkyta registracija.mygtukai dar neisiaiskinau su js

--- regilogi.js
+++ regilogi.js
@@ -1,8 +1,34 @@
 function login() { 
-    document.getElementById("login-btn").style.left = "-200px"; 
+    document.getElementById("xxxxx").style.left = "-200px"; 
     // document.getElementById("register-btn").style.left = "400px";
   }
-  function register() {
-    // document.getElementById("login-btn").style.left = "-40px";
-    document.getElementById("register-btn").style.left = "40px";
-  }  
+  // function register() {
+  //   // document.getElementById("login-btn").style.left = "-40px";
+  //   document.getElementById("register-btn").style.left = "40px";
+  // }  
+
+//   var f = document.getElementById("login-btn"); 
+//   var g = document.getElementById("register-btn"); 
+//   var h = document.getElementById("login"); 
+//   var i = document.getElementById("register"); 
+//   var j = document.getElementByClass("form-box"); 
+// function login(){  
+//     f.style.left = "-380px"; 
+//     g.style.left = "170px"; 
+//     h.style.left = "10px"; 
+//     i.style.left = "10px"; 
+//     j.style.left = "10px"; 
+//   } 
+
+//  var a = document.getElementById("login-btn"); 
+//  var b = document.getElementById("register-btn"); 
+//  var c = document.getElementById("login"); 
+//  var d = document.getElementById("register");
+//  var e = document.getElementByClass("form-box");
+//  function register(){ 
+//   a.style.left = "-1000px"; 
+//   b.style.left = "-50px"; 
+//   c.style.left = "100px"; 
+//   d.style.left = "100px"; 
+//   e.style.left = "100px"; 
+//  } 
