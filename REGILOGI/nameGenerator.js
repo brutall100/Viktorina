28b5acba979cd8efa4ddb8ef407d0426f6119afc
brutall100 <37956008+brutall100@users.noreply.github.com>
2022-12-11 18:13:36 +0200

    big
    Big update

--- REGILOGI/nameGenerator.js
+++ REGILOGI/nameGenerator.js
@@ -1,6 +1,6 @@
-const firstNames = ["Justė","AKiss","Ąžuolas","Kamilė","Dalia","Saulius","Juratė","Lukas","Dovydas","Ignas","Inga", "Laura", "Kristina", "Valdelis", "Marius", "Tania", "Rūta", "Gintarė", "Tomas", "Erlandas", "Giedrius", "Viktorija", "Ramas", "Efka", "Reno", "Rex", "Mama", "Tete", "Sese", "Teta"];
+let firstNames = ["Justė","AKiss","Ąžuolas","Kamilė","Dalia","Saulius","Juratė","Lukas","Dovydas","Ignas","Inga", "Laura", "Kristina", "Valdelis", "Marius", "Tania", "Rūta", "Gintarė", "Tomas", "Erlandas", "Giedrius", "Viktorija", "Ramas", "Efka", "Reno", "Rex", "Mama", "Tete", "Sese", "Teta"];
 
-const secondNames = ["Uoga","Pupa","Super","Linksmuolis","Bambalinis","Kukli","Paprastutė","Generolas","Nuolanki","Trapi","Išmintinga","Kietasis","1","2","3","","Sasiska","Puikioji","Išmanusis","Piktas","Rusas","Kiškis","Genijus","Babulė","Dundukas","Siaubūnas", "Protinguolis", "Afigienas", "Beprotis", "Juokingas", "Tikintis", "Storulis", "Beprotis", "Pilotas", "Trumparegis", "Mamos", "Koldūnas", "Sliekas", "Baršiasriubis", "Gudrus", "Gerasis", "Išprotejusi", "Karšta", "Gudri", "Putiovas","Digimonas", "Pokemonas", "Gražioji"];
+let secondNames = ["Uoga","Pupa","Super","Linksmuolis","Bambalinis","Kukli","Paprastutė","Generolas","Nuolanki","Trapi","Išmintinga","Kietasis","1","2","3","","Sasiska","Puikioji","Išmanusis","Piktas","Rusas","Kiškis","Genijus","Babulė","Dundukas","Siaubūnas", "Protinguolis", "Afigienas", "Beprotis", "Juokingas", "Tikintis", "Storulis", "Beprotis", "Pilotas", "Trumparegis", "Mamos", "Koldūnas", "Sliekas", "Baršiasriubis", "Gudrus", "Gerasis", "Išprotejusi", "Karšta", "Gudri", "Putiovas","Digimonas", "Pokemonas", "Gražioji"];
 
 let getRandomNumber = (max) => Math.floor(Math.random() * max);
 
@@ -17,49 +17,6 @@ document.getElementById('generuoti-varda')
 setRandomName();
 
 
-
-
-// 1 Dar bus galima irasyti vardo ir pavardes pasiulymus vartotojui turinciam lvl
-// 1 Kad vartotojas galetu kazka pasiulyti arba Registruojantis
-// 1 Jo nickas nueitu i pasiulymus let firstNames
-// 2  Auto copy mygtukas arba tiesiog paspaudus ant patinkancio nick
-// 2 nickas nusikeltu i registracija
-// funkcija kolkas neveikia
-
-
-// function autoCopy() {
-//   var copyText = document.getElementById("kopijuoti-varda");
-//   copyText.select();
-//   copyText.setSelectionRange(0, 99999)
-//   document.execCommand("copy");
-//   alert("Copied the text: " + copyText.value);
-// }
-
-
-// const copyToClipboard = str => {
-// 	let el = document.createElement('random-name');
-// 	el.value = str;
-// 	document.body.appendChild(el);
-// 	el.select();
-// 	document.execCommand('copy');
-// 	document.body.removeChild(el);
-//   }; 
-
-
-// clipboard.addEventListener('click', () => {
-// 	const textarea = document.createElement('textarea');
-// 	const password = resultEl.innerText;
-	
-// 	if(!password) { return; }
-	
-// 	textarea.value = password;
-// 	document.body.appendChild(textarea);
-// 	textarea.select();
-// 	document.execCommand('copy');
-// 	textarea.remove();
-// 	alert('Password copied to clipboard');
-// });
-
 const randomName = document.getElementById('random-name');
 const clipboard = document.getElementById('clipboard');
 
@@ -67,4 +24,18 @@ clipboard.onclick = function() {
     randomName.select();
 	document.execCommand('Copy');
   
+}
+
+
+
+
+function generatePassword() {
+  var password = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{};':" + "\\|,.<>/?`~";
+
+  for (var i = 0; i < 10; i++) {
+    password += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  document.getElementById("password").innerHTML = password;
 }
